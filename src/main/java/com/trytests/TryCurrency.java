4fcdbecc23e6f115a7df2523361dd3c10036af19
jchen <jchen@viator.com>
2017-06-29 09:14:39 +1000

    update TryCurrency

--- src/main/java/com/trytests/TryCurrency.java
+++ src/main/java/com/trytests/TryCurrency.java
@@ -1,8 +1,10 @@
 package com.trytests;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Currency;
+import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
@@ -13,7 +15,14 @@ public class TryCurrency {
 
     public static void main(String[] args) {
         Locale[] locales = Locale.getAvailableLocales();
-        Set<Currency> availableCurrencies = Currency.getAvailableCurrencies();
+        List<Currency> currencies = new ArrayList<>(Currency.getAvailableCurrencies());
+
+        Collections.sort(currencies, new Comparator<Currency>() {
+            @Override
+            public int compare(Currency o1, Currency o2) {
+                return o1.getCurrencyCode().compareTo(o2.getCurrencyCode());
+            }
+        });
 
         for(Locale l: locales){
             if( null == l.getCountry() || l.getCountry().isEmpty())
